3b6cc96ae9c9e2223b0b436a106cd1ef76725715
Guillermo Lapuente <guillermolapuente@gmail.com>
2016-09-17 15:38:41 +0200

    Included apps users, posts and categories to the project

--- practica/settings.py
+++ practica/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practica',
+    'posts',
+    'users',
+    'cagories',
 ]
 
 MIDDLEWARE = [
